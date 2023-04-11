c903c07dbddf35e1bc3c60e878d5b504ed4edab0
Marc Newman <marc.anthony.newman@gmail.com>
2023-04-11 11:18:10 -0400

    array

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -14,6 +14,10 @@ public class ArraysExercises {
 
         return longerArrayCopy;
     }
+
+    public ArraysExercises() {
+    }
+
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
